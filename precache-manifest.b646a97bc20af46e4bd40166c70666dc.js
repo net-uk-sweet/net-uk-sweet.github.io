73bb12fe0aa9a0d6d74a1a4632e9ffffecdfe220
Ian Watson <iwatson@williamhill.co.uk>
2019-12-13 19:27:35 +0000

    first deploy

---
+++ precache-manifest.b646a97bc20af46e4bd40166c70666dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1fc279b7d53109a8f04099f9f31eb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6c7c35b01e453ebf6ae",
+    "url": "/static/css/main.1f5c162d.chunk.css"
+  },
+  {
+    "revision": "baceb3e83d72e83fb5b9",
+    "url": "/static/js/2.d613719c.chunk.js"
+  },
+  {
+    "revision": "e6c7c35b01e453ebf6ae",
+    "url": "/static/js/main.b3f1d569.chunk.js"
+  },
+  {
+    "revision": "6e199ccc7a2a5a4d39d6",
+    "url": "/static/js/runtime-main.fa9fabfd.js"
+  }
+]);
